51041964f440e9fef58e9aa8b2b7b8af4462b1a9
Andriy258 <molchanovych.a.f_ipz24@rcit.ukr.education>
2025-09-15 17:28:52 +0300

    test2

--- lab2v9/Program.cs
+++ lab2v9/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Lab2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Inventory inventory = new Inventory();
+
+        inventory += "Молоко";
+        inventory += "Хліб";
+        inventory += "Сир";
+
+        Console.WriteLine(inventory);
+
+        Console.WriteLine("Товар з індексом 1: " + inventory[1]);
+
+        inventory[1] = "Булка";
+        Console.WriteLine("Після зміни: " + inventory);
+
+        string searchItem = "Сир";
+        int index = inventory[searchItem];
+        Console.WriteLine($"Товар \"{searchItem}\" має індекс: {index}");
+
+        Console.WriteLine("Загальна кількість товарів: " + inventory.Count);
+    }
+}
